e7ed9f937c4cd4e1eecca473cb6602ede6c03b3e
OscarGomezGonzalezz <oscgomgon.business@gmail.com>
2025-03-26 12:10:40 +0100

    removing last trials

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require("dotenv").config();//Load the .env file
 
 
 const authRoutes = require("./routes/authRoutes");
-const ovpnRoutes = require("./routes/ovpnRoutes");
+const testRoutes = require("./routes/testRoutes");
 
 const app = express();
 connectDB().then( async client => {
@@ -19,47 +19,7 @@ connectDB().then( async client => {
     app.use(express.json());
 
     app.use("/api/auth", authRoutes);
-    app.use("/api/ovpn", ovpnRoutes);
-
-    async function populateOvpnFiles() {
-        try {
-          const ovpnFilesCollection = db.collection('servers');
-      
-          const ovpnDir = path.join(__dirname, 'ovpnFiles'); // Directory containing .ovpn files
-          const files = fs.readdirSync(ovpnDir).filter(file => file.endsWith('.ovpn'));
-          console.log(files);
-      
-          for (const file of files) {
-            const filePath = path.join(ovpnDir, file);
-            const content = fs.readFileSync(filePath, 'base64'); // Read and encode content to Base64
-      
-            const serverName = path.basename(file, '.ovpn'); // Use filename (without extension) as server name
-      
-            const existingFile = await ovpnFilesCollection.findOne({ fileName: file });
-            if (!existingFile) { // Avoid duplicates
-              await ovpnFilesCollection.insertOne({
-                serverName,
-                fileName: file,
-                content, // Store Base64 content
-                createdAt: new Date(),
-              });
-              console.log(`Inserted ${file} into MongoDB`);
-            } else {
-              console.log(`File ${file} already exists in the database.`);
-            }
-          }
-        } catch (error) {
-          console.error('Error populating .ovpn files:', error);
-        }
-      }
-
-    // **Store VPN Configurations on Server Startup**
-    try {
-        await populateOvpnFiles(); // Populate the database on server start
-        
-    } catch (error) {
-        console.error("Error inserting configurations:", error);
-    }
+    app.use("/api/test", testRoutes);
 
     app.listen(3500, () => console.log("Server running on port 3500"));
 
